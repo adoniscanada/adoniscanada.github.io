672d041569e67dd4f3d837000249476749620c30
Adonis Canada <adonis.a.canada@gmail.com>
2026-06-15 14:42:16 -0400

    configured CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://adoniscanada.github.io",
+  site: "https://www.canadaa.org",
   vite: {
     plugins: [tailwindcss()],
   },
